7db789ee72dd28a7d80774978092325e10c633bb
karan-parekh <karanparekh501@gmail.com>
2020-05-11 10:38:37 +0530

    minor changes

--- rock_paper_scissors/server.py
+++ rock_paper_scissors/server.py
@@ -3,7 +3,7 @@ import zmq
 from typing import Optional
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket  = context.socket(zmq.REP)
 
 print("BINDING TO PORT: 5555")
 socket.bind("tcp://*:5555")
@@ -23,10 +23,10 @@ class Game:
 
     def __init__(self):
 
-        self.id       = 0
-        self.players  = 0
-        self.player_1 = ''
-        self.player_2 = ''
+        self.id      = 0
+        self.players = 0
+        self.p1_move = ''
+        self.p2_move = ''
 
 
 games = {}
@@ -34,56 +34,44 @@ games = {}
 
 def get_winner(game: Game) -> dict:
 
-    p_beat_r = "PAPER WRAPS ROCK"
-    r_beat_s = "ROCK BREAKS SCISSORS"
-    s_beat_p = "SCISSORS CUT PAPER"
-
     win = {
         'player': 0,
         'reason': ''
     }
 
-    if not game.player_1 or not game.player_2:
+    if not game.p1_move or not game.p2_move:
         return win
 
-    if game.player_1 == 'p' and game.player_2 == 'r':
-        win['player'] = 1
-        win['reason'] = p_beat_r
-        return win
+    p_beat_r = "PAPER WRAPS ROCK"
+    r_beat_s = "ROCK BREAKS SCISSORS"
+    s_beat_p = "SCISSORS CUT PAPER"
 
-    if game.player_1 == 'r' and game.player_2 == 'p':
-        win['player'] = 2
-        win['reason'] = p_beat_r
-        return win
+    state = {
+        game.p1_move : 1,
+        game.p2_move : 2,
+    }
 
-    if game.player_1 == 'r' and game.player_2 == 's':
-        win['player'] = 1
-        win['reason'] = r_beat_s
-        return win
+    chances = {
+        {'p', 'r'}: {'move': 'p', 'reason': p_beat_r},
+        {'r', 's'}: {'move': 'r', 'reason': r_beat_s},
+        {'s', 'p'}: {'move': 's', 'reason': s_beat_p}
+    }
 
-    if game.player_1 == 's' and game.player_2 == 'r':
-        win['player'] = 2
-        win['reason'] = r_beat_s
-        return win
+    c = chances[{game.p1_move, game.p2_move}]
 
-    if game.player_1 == 's' and game.player_2 == 'p':
-        win['player'] = 1
-        win['reason'] = s_beat_p
-        return win
+    win['player'] = state[c['move']]
+    win['reason'] = c['reason']
 
-    if game.player_1 == 'p' and game.player_2 == 's':
-        win['player'] = 2
-        win['reason'] = s_beat_p
-        return win
+    return win
 
 
 def set_move(turn: dict, game: Game) -> Game:
 
     if turn['player_id'] == 1:
-        game.player_1 = turn['move']
+        game.p1_move = turn['move']
 
     if turn['player_id'] == 2:
-        game.player_2 = turn['move']
+        game.p2_move = turn['move']
 
     return game
 
@@ -116,59 +104,62 @@ def create_game() -> Game:
     return g
 
 
-while True:
+if __name__ == '__main__':
 
-    obj = socket.recv_pyobj()
-    command = obj['cmd']
+    while True:
 
-    if command == 'create':
-        g = create_game()
-        p = Packet()
+        obj     = socket.recv_pyobj()
+        command = obj['cmd']
 
-        p.game_id   = g.id
-        p.player_id = 1
+        if command == 'create':
+            g = create_game()
+            p = Packet()
 
-        socket.send_pyobj(p)
-        continue
-
-    if command == 'join':
-        g = get_game(obj['id'])
-        p = Packet()
-
-        if not g:
-            socket.send_pyobj(p)
-            continue
-
-        if g.players >= 2:
             p.game_id   = g.id
-            p.game_full = True
+            p.player_id = 1
 
             socket.send_pyobj(p)
             continue
 
-        p.game_id   = g.id
-        p.player_id = 2
+        if command == 'join':
+            g = get_game(obj['game_id'])
+            p = Packet()
 
-        socket.send_pyobj(p)
-        continue
+            if not g:
+                socket.send_pyobj(p)
+                continue
 
-    if command == 'play':
-        g = set_move(obj, get_game(obj['id']))
-        p = Packet()
+            if g.players >= 2:
+                p.game_id   = g.id
+                p.game_full = True
 
-        winner = get_winner(g)
+                socket.send_pyobj(p)
+                continue
 
-        p.game_id   = g.id
-        p.player_id = obj['player_id']
+            p.game_id   = g.id
+            p.player_id = 2
 
-        if not winner['player']:
             socket.send_pyobj(p)
             continue
 
-        p.winner = winner
+        if command == 'play':
+            g = set_move(obj, get_game(obj['game_id']))
+            p = Packet()
 
-        socket.send_pyobj(p)
-        continue
+            winner = get_winner(g)
 
-    if command == 'stop':
-        break
+            p.game_id   = g.id
+            p.player_id = obj['player_id']
+
+            if not winner['player']:
+                socket.send_pyobj(p)
+                continue
+
+            p.winner = winner
+
+            socket.send_pyobj(p)
+            continue
+
+        if command == 'stop':
+            print("STOPPING SERVER")
+            break
