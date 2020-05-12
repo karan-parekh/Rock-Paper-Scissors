a535234f19619eac77aa3c80c6c18df174369f5c
karan-parekh <karanparekh501@gmail.com>
2020-05-12 10:42:41 +0530

    added class Packet and handles full games

--- rock_paper_scissors/player.py
+++ rock_paper_scissors/player.py
@@ -8,6 +8,16 @@ socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 
+class Packet:
+
+    def __init__(self):
+
+        self.game_id   = 0
+        self.player_id = 0
+        self.winner    = {}
+        self.game_full = False
+
+
 packet = {
     'cmd'       : '',
     'game_id'   : 0,
@@ -27,14 +37,17 @@ def main():
 
     while True:
 
-        cmd = input("PRESS C TO CREATE GAME \nPRESS J TO JOIN\n:").lower()
+        cmd = input("PRESS C TO CREATE GAME\nPRESS J TO JOIN\n:").lower()
 
         if cmd == 'c':
             print("CREATING GAME")
             packet['cmd'] = 'create'
             socket.send_pyobj(packet)
 
-            return socket.recv_pyobj()
+            p = socket.recv_pyobj()
+            print("YOU CREATED GAME WITH ID: {}".format(p.game_id))
+
+            return p
 
         if cmd == 'j':
             packet['cmd']     = 'join'
@@ -43,7 +56,13 @@ def main():
 
             socket.send_pyobj(packet)
 
-            return socket.recv_pyobj()
+            p = socket.recv_pyobj()
+
+            if p.game_full:
+                print("GAME IS FULL. TRY ANOTHER ID OR CREATE A NEW GAME")
+                continue
+
+            return p
 
         print("TRY AGAIN")
 
@@ -58,7 +77,7 @@ def wait_for_response(p):
         res = socket.recv_pyobj()
 
         if not res.winner:
-            time.sleep(0.25)
+            time.sleep(1)
             continue
 
         return res.winner
@@ -101,6 +120,9 @@ if __name__ == '__main__':
 
         if winner['player'] == state['player_id']:
             print("YOU WIN")
+
+        elif winner['player'] == -1:
+            print("NO WINNER")
 
         else:
             print("YOU LOSE")
